80fc14819eb4243ff51c1f09a3bb685055e7c825
Lev Myshkin <student.lev.myshkin@gmail.com>
2023-04-13 01:52:08 +0300

    solved 3 task

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,10 +14,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+
+ let arr = [
+  ["^^", ".", null, 0, false, "", NaN, "^^", 2, true, "dasdas", 1],
+  [2, null, 0, 1, ".", "dasdas", true, NaN, "", false, "^^", "^^"],
+  [false, ".", 1, 0, "^^", null, "", 2, "dasdas", "^^", NaN, true],
+  [".", false, 1, null, NaN, 2, 0, "dasdas", true, "^^", "", "^^"],
+  [false, ".", 1, 0, "^^", true, null, "^^", "", NaN, 2, "dasdas"],
+  [false, NaN, 1, 0, ".", "^^", null, true, "dasdas", "^^", 2, ""],
+  [null, 1, NaN, true, ".", "^^", "^^", 2, "", false, "dasdas", 0],
+  [null, NaN, "", false, ".", 1, 0, "^^", "dasdas", true, 2, "^^"],
+]
+
+function countCats( matrix ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let target = '^^';
+  let result = [];
+  matrix.forEach(array => {
+    let test;
+    if(Array.isArray(array)) {
+      test = array.filter(item => item === target);
+      if(test.length) result.push(...test);
+    }
+  })
+  return result.length;
 }
+
 
 module.exports = {
   countCats
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,10 +11,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+
+let digit = 152;
+
+function deleteDigit( n ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const digits = n.toString().split('');
+  let max = 0;
+  for (let i = 0; i < digits.length; i++) {
+    let tmp = [...digits];
+    tmp.splice(i, 1);
+    const num = parseInt(tmp.join(''));
+    if(num > max) max = num
+  }
+  return max
 }
+
 
 module.exports = {
   deleteDigit
